34dc7c8e68f3b032669e73bbc65d48edfc98a6e9
Gandhi0301 <acg03104@gmail.com>
2025-09-12 18:26:19 +0530

    made changes in the Product Card by removing borders, maintaining the aspect ratio so that the size of card remains same and looks good

--- frontend/src/components/ProductCard/ProductCard.jsx
+++ frontend/src/components/ProductCard/ProductCard.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-router-dom";
 const ProductCard = ({product}) => {
     return (
         <Link to={`/product/${product._id}`}>
-        <div className="flex flex-col items-center justify-center border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+        <div className="flex flex-col items-center justify-between z-50 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 aspect-[4/5]">
             <div>
-                <img src={product.imageUrl} alt={product.name} className="w-full aspect-square object-cover rounded-t-lg shadow-md z-10" />
+                <img src={product.imageUrl} alt={product.name} className="w-full  object-cover rounded-t-lg shadow-md z-10" />
             </div>
-            <div className="flex flex-col items-left align-middle w-full">
+            <div className="flex flex-col items-left align-middle w-full mb-1">
                 <div className="mt-2 px-2 text-sm md:text-md lg:text-lg font-semibold overflow-hidden text-ellipsis whitespace-nowrap">
                     {product.name}
                 </div>
